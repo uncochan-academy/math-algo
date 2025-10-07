60915144d62408bddf6ca08ac3d0ebe12f7433c8
uncochan-academy <nishidamailyou@gmail.com>
2025-10-07 11:21:47 +0900

    ユークリッドの互除法を追加しました

---
+++ kari/euclid/target/debug/deps/euclid.d
@@ -0,0 +1,5 @@
+C:\projects\math-algo\kari\euclid\target\debug\deps\euclid.d: src\main.rs
+
+C:\projects\math-algo\kari\euclid\target\debug\deps\euclid.exe: src\main.rs
+
+src\main.rs:
---
+++ kari/euclid/target/debug/euclid.d
@@ -0,0 +1,1 @@
+C:\projects\math-algo\kari\euclid\target\debug\euclid.exe: C:\projects\math-algo\kari\euclid\src\main.rs
