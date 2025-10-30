73e3bf498ab848b7aee2547cdc39a9ff4ca2108a
uncochan-academy <nishidamailyou@gmail.com>
2025-10-30 12:38:56 +0900

    改良した

--- kari/field32/src/field32.rs
+++ kari/field32/src/field32.rs
@@ -10,20 +10,10 @@ impl Mul for MyNumber {
     type Output = MyNumber;
     
     fn mul(self, other: MyNumber) -> MyNumber {
-        let mut result: u64 =0;
 
-        for i in 0..32 {
-            if (other.value >> i) & 1 == 1 {
-                result = result ^ ((self.value as u64) << i);
-            }
-        }
+        let mut result = kakezan(self.value, other.value);
 
-        for i in 0..32 {
-            if (result >> (63 - i)) & 1 == 1 {
-                result = result ^ (IRREDUCIBLE_POLYNOMIAL << (31 - i));
-            }
-        }
-
+        result = poly_warizan(result,IRREDUCIBLE_POLYNOMIAL);
 
         MyNumber {
             value: result as u32,
@@ -33,4 +23,28 @@ impl Mul for MyNumber {
 
 pub fn create_number(value: u32) -> MyNumber {
     MyNumber { value }
+}
+
+pub fn kakezan(a: u32, b: u32) -> u64 {
+    //aかけるb
+    let mut result: u64 =0;
+    for i in 0..32 {
+            if (b >> i) & 1 == 1 {
+                result = result ^ ((a as u64) << i);
+            }
+    }
+    return result;
+}
+
+pub fn poly_warizan(mut a: u64, b: u64) -> u64 {
+    //aわるbのあまり,bが０でないことは検証しないから注意
+    //多項式bの最高次の次元dimを求める。
+    let dim = 63 - b.leading_zeros();
+    for i in 0..(64 - dim) {
+        if (a >> (63 - i)) & 1 == 1 {
+            a = a ^ (b << (63 - dim - i));
+        }
+    }
+    return a;
+      
 }
