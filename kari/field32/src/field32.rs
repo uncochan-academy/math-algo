cf5d37356ac9a972d6c47aec1d81fff2039ca576
uncochan-academy <nishidamailyou@gmail.com>
2025-10-30 16:29:53 +0900

    逆元を出力する関数を作っている途中

--- kari/field32/src/field32.rs
+++ kari/field32/src/field32.rs
@@ -1,4 +1,5 @@
 use std::ops::Mul;
+//https://www.partow.net/programming/polynomials/index.html
 const IRREDUCIBLE_POLYNOMIAL: u64 = 0x1_0040_0007;
 
 #[derive(Debug, Clone, Copy)]//なんかよくわからんもの
@@ -11,9 +12,9 @@ impl Mul for MyNumber {
     
     fn mul(self, other: MyNumber) -> MyNumber {
 
-        let mut result = kakezan(self.value, other.value);
+        let mut result = kakezan(self.value as u64, other.value as u64);
 
-        result = poly_warizan(result,IRREDUCIBLE_POLYNOMIAL);
+        (_,result) = poly_warizan(result,IRREDUCIBLE_POLYNOMIAL);
 
         MyNumber {
             value: result as u32,
@@ -25,26 +26,44 @@ pub fn create_number(value: u32) -> MyNumber {
     MyNumber { value }
 }
 
-pub fn kakezan(a: u32, b: u32) -> u64 {
-    //aかけるb
+pub fn kakezan(a: u64, b: u64) -> u64 {
+    //aかけるb.u32までしかちゃんと動かない
     let mut result: u64 =0;
-    for i in 0..32 {
+    for i in 0..64 {
             if (b >> i) & 1 == 1 {
-                result = result ^ ((a as u64) << i);
+                result = result ^ (a << i);
             }
     }
     return result;
 }
 
-pub fn poly_warizan(mut a: u64, b: u64) -> u64 {
+pub fn poly_warizan(a: u64, b: u64) -> (u64,u64) {
     //aわるbのあまり,bが０でないことは検証しないから注意
     //多項式bの最高次の次元dimを求める。
+    let mut syou:u64 = 0;
+    let mut amari:u64 = a;
     let dim = 63 - b.leading_zeros();
     for i in 0..(64 - dim) {
-        if (a >> (63 - i)) & 1 == 1 {
-            a = a ^ (b << (63 - dim - i));
+        if (amari >> (63 - i)) & 1 == 1 {
+            amari = amari ^ (b << (63 - dim - i));
+            syou = syou ^ (1 << (63 - dim - i));
         }
     }
-    return a;
-      
+    return (syou,amari);      
+}
+ 
+pub fn inverse(a: MyNumber) -> MyNumber {
+    let (mut q, mut r):(u64,u64) = (a.value as u64,IRREDUCIBLE_POLYNOMIAL);
+    (q,r) = poly_warizan(q,r);
+    let mut v: Vec<u64> = vec![0,1,1,q];
+    while r != 0 {
+        (q,(_,r)) = (r,poly_warizan(q,r));
+        let mut temp: u64 = v[2];
+        v[2] = v[0] ^ kakezan(q,v[1]);
+        v[0] = temp;
+        temp = v[3];
+        v[3] = v[1] ^ kakezan(q,v[2]);
+        v[1] = temp;
+    }
+    return MyNumber { value: v[0] as u32 };
 }
--- kari/field32/src/main.rs
+++ kari/field32/src/main.rs
@@ -1,8 +1,13 @@
 mod field32;
 use field32::*;
 fn main() {
-    let a = create_number(3);
-    let b = create_number(3);
+    let a = create_number(1635);
+    let b = create_number(2432);
     let result = a * b;
-    println!("3 * 3 = {}", result.value);
+    println!("1635*2472 = {}", result.value);
+
+    let c: MyNumber = create_number(3);
+    let result2 = inverse(c);
+    println!("3の逆元 = {}", result2.value);
+
 }
